de26e4b688a6bcd759f7cea6aa71696e76f91c95
Leo Perugini <xweetyk@gmail.com>
2018-08-07 04:01:01 -0300

    Menu Updated
    -Main menu is now working

--- SC2-The Other Side/SC2-The Other Side/Game.cpp
+++ SC2-The Other Side/SC2-The Other Side/Game.cpp
@@ -1,27 +1,132 @@
 #include "Game.h"
-
-Game::Game()
-{
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+Game::Game(){
+	gameState = MENU;
+	init();
+}
+Game::~Game(){
+}
+void Game::init() {
+	window.create(VideoMode(800, 500), "Star Chaser 2: The Other Side");
+	select = 1;
+	update();
+}
+void Game::update() {
+	switch (gameState)
+	{
+	case MENU:
+		menu();
+		break;
+	case PLAY:
+		play();
+		break;
+	case SCORES:
+		score();
+		break;
+	case RESULT:
+		result();
+	case EXIT:
+		window.close();
+		break;
+	}
+}
+void Game::menu() {
+	menuBg.loadFromFile("../Assets/BG/bg.png");
+	mBG.setTexture(menuBg);
+	menuLogo.loadFromFile("../Assets/menu/logo.png");
+	mLogo.setTexture(menuLogo);
+	mLogo.setPosition(210, 80);
+	menuSelect1.loadFromFile("../Assets/menu/button0.png");
+	menuSelect2.loadFromFile("../Assets/menu/button1.png");
+	menuSelect3.loadFromFile("../Assets/menu/button2.png");
+	mSelect.setPosition(295, 400);
 
 	while (window.isOpen())
 	{
-		sf::Event event;
+		Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			if (event.type == Event::Closed)
+				window.close();
+		}
+		switch (select)
+		{
+		case 1:
+			mSelect.setTexture(menuSelect1);
+			break;
+		case 2:
+			mSelect.setTexture(menuSelect2);
+			break;
+		case 3:
+			mSelect.setTexture(menuSelect3);
+			break;
+		}
+		switch (event.type)
+		{
+		case sf::Event::KeyPressed:
+			switch (event.key.code)
+			{
+			case Keyboard::Left:
+				if (select>1)
+					select--;
+				break;
+			case Keyboard::Right:
+				if (select<3)
+					select++;
+				break;
+			case Keyboard::Space:
+				switch (select)
+				{
+				case 1:
+					gameState = PLAY;
+					break;
+				case 2:
+					gameState = SCORES;
+					break;
+				case 3:
+					gameState = EXIT;
+					break;
+				}
+			}
+			break;
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Space)) {
+			switch (select)
+			{
+			case 1:
+				gameState = PLAY;
+				break;
+			case 2:
+				gameState = SCORES;
+				break;
+			case 3:
+				gameState = EXIT;
+				break;
+			}
+		}
+		cout << gameState;
+		window.clear();
+		window.draw(mBG);
+		window.draw(mLogo);
+		window.draw(mSelect);
+		window.display();
+		sleep(seconds(0.1f));
+		update();
+	}
+}
+
+void Game::play() {
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
 				window.close();
 		}
 
 		window.clear();
-		window.draw(shape);
 		window.display();
 	}
 }
-
-
-Game::~Game()
-{
-}
+void Game::result() {}
+void Game::score() {}
--- SC2-The Other Side/SC2-The Other Side/Game.h
+++ SC2-The Other Side/SC2-The Other Side/Game.h
@@ -3,10 +3,33 @@
 #include <iostream>
 #include <ctime>
 #include <SFML/Graphics.hpp>
-class Game
-{
+using namespace std;
+using namespace sf;
+class Game{
+	enum GameState {MENU, PLAY, RESULT, SCORES, EXIT};
+	GameState gameState;
+	RenderWindow window;
+	Font font;
+	//Menu---------------
+	Texture menuBg;
+	Texture menuLogo;
+	Texture menuSelect1;
+	Texture menuSelect2;
+	Texture menuSelect3;
+	Sprite mBG;
+	Sprite mLogo;
+	Sprite mSelect;
+	int select;
+	//-------------------
+	
 public:
 	Game();
 	~Game();
+	void init();
+	void update();
+	void menu();
+	void play();
+	void result();
+	void score();
 };
 #endif
